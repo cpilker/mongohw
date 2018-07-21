67cfff1477b2d9f5942a788e71671273f026cd43
Chad Pilker <chad.pilker@gmail.com>
2018-07-21 12:47:32 -0400

    updated connection

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI ||"mongodb://localhost/mongohw";
+var MONGODB_URI = process.env.MONGODB_URI //||"mongodb://localhost/mongohw";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 //Routes here
